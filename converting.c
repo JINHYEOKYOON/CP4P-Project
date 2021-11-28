38e63bc2136911077591d92e5b9445c7a5dab7df
JINHYEOKYOON <jyoon42@myseneca.ca>
2021-11-28 02:39:15 -0500

    v1

--- converting.c
+++ converting.c
@@ -1,7 +1,22 @@
 #include "converting.h"
 
 void converting() {
+
 /* Version 1 */
+
+	printf("*** Start of Converting Strings to int Demo ***\n);
+	char int_string[80];
+	int int_number;
+	printf("Type the int numeric string (q - to quit):\n");
+	gets(int_string);
+	while (strcmp(int_string, "q") !=0) {
+		int_number = atoi(int_string);
+		printf("Converted number is %d", int_number);
+		printf("Type the int numeric string (q - to quit):\n");
+		gets(int_string);
+	}printf("*** End of Converting Strings to int Demo ***\n\n");
+}
+	
 >> insert here
 
 
